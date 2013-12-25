6dcaf3e4390e8fca30665636c13140274b91c210
gyollin (Guy Yollin) <gyollin@gmail.com>
2013-12-25 07:26:38 +0000

    Add new demos
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sdafe@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/demo/fig_2.2.R
@@ -0,0 +1,13 @@
+S0 <- 0
+mu <- 0.5
+sigma <- 1
+tm <- seq(0,11,len=100)
+S <- S0 + mu*tm
+ubound <- S0 + mu*tm + sigma*sqrt(tm)
+lbound <- S0 + mu*tm - sigma*sqrt(tm)
+plot(x=tm,y=S,xlab="time",ylab="price",main="random walk",
+     type="l",lwd=2,ylim=c(-1,9),xlim=c(0,10),col=4)
+lines(tm,ubound,lty=2,lwd=3,col=2)
+lines(tm,lbound,lty=3,lwd=3,col=2)
+legend(x="top",c("mean","mean + sd","mean - sd"),
+       lty = c(1,2,3),lwd=2,bty="n",col=c(4,2,2))
